e2d7e00afc68fa9b43aba53d9929fc6109b6d782
Jonatan Angergård <jonteufrisbee@gmail.com>
2022-12-05 14:31:17 +0100

    day 5 complete

---
+++ code/5dec/script2.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+fs.readFile(
+  "/Users/jonatanangergard/AOC/code/5dec/input.txt",
+  "utf-8",
+  (err, text) => {
+    if (err) {
+      console.log(err);
+      console.log(err.message);
+      return;
+    }
+    const data = JSON.stringify(text).replace(/\"/g, "").split("\\n");
+    const moves = [];
+    for (const line of data) {
+      const cut = line
+        .replace("move ", "")
+        .replace(" from ", ",")
+        .replace(" to ", ",")
+        .split(",");
+      moves.push(cut);
+    }
+    const stack = stacks;
+    console.log(stack);
+    for (const move of moves) {
+      const from = +move[1];
+      const to = +move[2];
+      const num = +move[0];
+      const slice = stack[from].slice(stack[from].length - num);
+      console.log(slice);
+      for (let i = 0; i < num; i++) {
+        stack[from].pop();
+      }
+      for (const char of slice) {
+        stack[to].push(char);
+      }
+    }
+    console.log(stack);
+  }
+);
+
+const stacks = {
+  1: ["N", "C", "R", "T", "M", "Z", "P"],
+  2: ["D", "N", "T", "S", "B", "Z"],
+  3: ["M", "H", "Q", "R", "F", "C", "T", "G"],
+  4: ["G", "R", "Z"],
+  5: ["Z", "N", "R", "H"],
+  6: ["F", "H", "S", "W", "P", "Z", "L", "D"],
+  7: ["W", "D", "Z", "R", "C", "G", "M"],
+  8: ["S", "J", "F", "L", "H", "W", "Z", "Q"],
+  9: ["S", "Q", "P", "W", "N"],
+};
